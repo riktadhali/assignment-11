3bca19b830eb9e038def2d8eee4da2df258fa887
riktadhali <riktadhali031@gmail.com>
2024-11-10 23:03:01 +0600

    first commit

---
+++ if-else.js
@@ -0,0 +1,28 @@
+// var hour = 22;
+// if(hour<18){
+//     console.log("Good day");
+// }else{
+//     console.log("hard day");
+    
+// }
+
+// var he = 29;
+// var she = 34;
+// if(he<30){
+//     console.log("Good");
+// }else if(she>32){
+//     console.log("not bad"); 
+// }else{
+//     console.log("minus");    
+// }
+
+// var numbers = [21,23,34,45,54,56,67,78,87,89,98,95];
+// for (var index = 0; index < numbers.length; index++) {
+//     var element = numbers[index];
+//     console.log(element);    
+// }
+
+for (let index = 0; index < 16; index++) {
+  console.log("thank you");
+  console.log(index);   
+}
